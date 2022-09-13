d2ca06f12efc7de66d4e9576e7445bf78f3a6b31
JuanCarlos Roa Jr <roajrjuancarlos@hotmail.com>
2022-09-12 22:27:40 -0400

    updated javascript

---
+++ questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Inside which HTML element do we put the JavaScript?",
+    answer: "script",
+    options: [
+      "script",
+      "javascript",
+      "js",
+      "scripting"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Where is the correct place to insert a JavaScript?",
+    answer: "Both the head and the body section",
+    options: [
+      "The body section",
+      "Both the head and the body section",
+      "the head section",
+      "the title section"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The external JavaScript file must contain the script tag.",
+    answer: "False",
+    options: [
+      "True",
+      "False",
+      "I don't know",
+      "Both"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How to write an IF statement in JavaScript?",
+    answer: "IF (i == 5)",
+    options: [
+      "IF i = 5",
+      "IF i = 5 then",
+      "IF (i == 5)",
+      "IF i == 5 then"
+    ]
+  },
+    {
+    numb: 5,
+    question: "JavaScript is the same as Java.",
+    answer: "False",
+    options: [
+      "True",
+      "False",
+      "You can't spell JavaScript without Java",
+      "No clue"
+    ]
+  },
+
+];
